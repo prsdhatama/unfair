6d01a370aa5c6126ea79e08e3e1c92f6016d8a92
Wahyudi Prasidhatama <wahyudiprasidhatama@gmail.com>
2023-12-26 15:56:38 +0700

    add hero header animation

--- custom.js
+++ custom.js
@@ -1,56 +1,66 @@
-$(document).ready(function(){
-    document.addEventListener("DOMContentLoaded", function () {
-      var collapsibleButton = document.getElementById("collapsibleButton");
-  
-      // Get the coordinates
-      var rect = collapsibleButton.getBoundingClientRect();
-  
-      // Save the coordinates as variables
-      var buttonTop = rect.top;
-      var buttonLeft = rect.left;
-  
-      // Use these variables as needed in other elements or scripts
-      console.log("Button Top:", buttonTop);
-      console.log("Button Left:", buttonLeft);
-    });
-  });
-
-
 $(document).ready(function () {
+  // COLLAPSE BUTTON
+  $("#collapsibleButton").click(function () {
+    var collapsibleHeader = $("#collapsibleHeader");
 
-//COLLAPSE BUTTON
-    $("#collapsibleButton").click(function () {
-      if ($("#collapsibleHero").is(":visible")) {
-  
-        $("#collapsibleHero").hide();
-        $("#navbar").append('<a id="pseudo-button" class="pseudo-button" type="button"> <i class="ri-arrow-down-double-line"></i> </a>');
-      } else{}
-    });
+    if (collapsibleHeader.is(":visible")) {
+      // Use slideUp for a sliding animation when hiding
+      collapsibleHeader.slideUp(150);
 
-    $("#navbar").on("click", "#pseudo-button", function () {
-      $("#collapsibleHero").show();
-      $("#navbar .pseudo-button").remove();
-    });
+      // Append pseudo-button with a delay
+      $("#navbar").delay(150).queue(function (next) {
+        $(this).append('<a id="pseudo-button" class="pseudo-button" type="button"> <i class="ri-arrow-down-double-line"></i> </a>');
+        next();
+      });
+    }
   });
-  
 
+  // PSEUDO-BUTTON CLICK HANDLER
+  $("#navbar").on("click", "#pseudo-button", function () {
+    // Use slideDown for a sliding animation when showing
+    $("#collapsibleHeader").slideDown(150);
 
-$(document).ready(function(){
-  //STICKY NAVBAR
-  var num = 600;
-
-  $(window).bind('scroll', function () {
-      if ($(window).scrollTop() > num ) {
-        $("#collapsibleHero").hide();
-        $("#navbar").addClass("fixed-top");
-        $("#navbar").append('<a id="pseudo-button" class="pseudo-button" type="button"> <i class="ri-arrow-down-double-line"></i> </a>');
-      } else {
-        $("#collapsibleHero").show();
-        $("#navbar").removeClass("fixed-top");
-        $("#navbar .pseudo-button").remove();
-      }
+    // Remove pseudo-button with a delay
+    $("#navbar .pseudo-button").fadeOut(50, function () {
+      $(this).remove();
+    });
   });
 });
 
+  
 
+$(document).ready(function () {
+  // STICKY NAVBAR AND SNAP TO NEXT SECTION
+  var num = 550;
+  var sectionOffset = 600; // Adjust this value as needed
+
+  $(window).bind('scroll', function () {
+      var scrollTop = $(window).scrollTop();
+
+      if (scrollTop > num) {
+          $("#collapsibleHero").slideUp(150);
+          $("#navbar").addClass("fixed-top");
+
+          // Append pseudo-button with the same horizontal position
+          $("#navbar").append('<a id="pseudo-button" class="pseudo-button" type="button"> <i class="ri-arrow-down-double-line"></i> </a>');
+      } else {
+          $("#collapsibleHero").slideDown(150);
+          $("#navbar").removeClass("fixed-top");
+          $("#navbar .pseudo-button").remove();
+      }
+
+      // Check if scrolling is above the num and show the collapsible hero again
+      if (scrollTop < num) {
+          $("#collapsibleHero").slideDown(150);
+      }
+
+      // Optionally, you can snap to the next section when scrolling down
+      // if (scrollTop > num + sectionOffset) {
+      //     var nextSection = $("#section__container choose__container"); // Replace with the actual ID of your next section
+      //     $.scrollTo(nextSection, 800, { offset: -80 }); // Adjust the offset as needed
+      // }
+  });
+});
+
+  
 
